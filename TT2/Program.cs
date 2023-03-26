0cf0e923535a1c4bb0f4aa1e5827d68c993b4a8c
Alexander <fiireex@yandex.ru>
2023-03-26 15:11:56 +0300

    Done TT2

---
+++ TT2/Program.cs
@@ -0,0 +1,25 @@
+﻿void NewArray (int[]a)
+{
+    for (int i = 0; i < a.Length; i++)
+        a[i] = new Random().Next(1, 101);
+}
+
+int SUMMA (int[] b)
+{
+    int sum = 0;
+        {
+            for (int i = 1; i < b.Length; i+= 2)
+            sum += b[i];
+        }  
+    return sum;
+
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество элеметов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int []q = new int [n];
+NewArray(q);
+Console.WriteLine($"Стартовый массив: [{string.Join(", ", q)}]");
+Console.WriteLine($"Сумма элементов на нечётных позициях: {SUMMA(q)}");
+
